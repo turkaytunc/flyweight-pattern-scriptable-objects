8029f6e145796f2adade74106338ca31d24ebaaa
Turkay Tunc <turkaytunc00@gmail.com>
2020-06-05 10:15:37 +0300

    added weapon script

---
+++ Assets/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private string weaponName;
+    private string weaponDescription;
+    private int dropRate;
+    private int rarity;
+
+    private int damageAmount;
+    private float attackSpeed;
+    private string impueType;
+
+}
